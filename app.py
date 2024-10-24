f9fee924739cc6f0368146c4d81cd280c048e973
lusesser <lulusignor@gmail.com>
2024-10-24 16:28:00 +0200

    Please commit

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ hist_button = st.button('Criar histograma') # criar um botão
 
 st.header('Dados de Anúncio de Vendas')
 if hist_button: # se o botão for clicado
-            # escrever uma mensagem
-            st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+# escrever uma mensagem
+  st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
             
-            # criar um histograma
-            fig = px.histogram(car_data, x="odometer")
+ # criar um histograma
+  fig = px.histogram(car_data, x="odometer")
         
-            # exibir um gráfico Plotly interativo
-            st.plotly_chart(fig, use_container_width=True)
-
-import streamlit as st
+# exibir um gráfico Plotly interativo
+  st.plotly_chart(fig, use_container_width=True)
 
 # criar uma caixa de seleção
 build_histogram = st.checkbox('Criar um histograma')
